ceae33ab73a65f20f6ef5d65111c9878b74207b3
NamNV609 <namnv609@gmail.com>
2018-05-20 15:10:03 +0700

    Add dynamic layout

--- angular/src/app/app.routings.ts
+++ angular/src/app/app.routings.ts
@@ -1,8 +1,21 @@
 import { RouterModule, Routes } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core/src/metadata/ng_module";
 import { RegisterComponent } from "./components/register/register.component";
+import { SiteLayoutComponent } from "./_layout/site-layout/site-layout.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 export const AppRoutes: Routes = [
+  {
+    path: "",
+    component: SiteLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: DashboardComponent,
+        pathMatch: "full"
+      }
+    ]
+  },
   {
     path: "register",
     component: RegisterComponent
