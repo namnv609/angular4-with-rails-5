e4a40786f3e9c2711b71fd9d51ccd0fc0c79c479
NamNV609 <namnv609@gmail.com>
2018-05-20 10:47:27 +0700

    Config Angular router (without hash)

---
+++ angular/src/app/app.routings.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core/src/metadata/ng_module";
+import { RegisterComponent } from "./components/register/register.component";
+
+export const AppRoutes: Routes = [
+  {
+    path: "register",
+    component: RegisterComponent
+  }
+];
+export const AppRoutings: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
